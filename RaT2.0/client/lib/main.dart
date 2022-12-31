6977bf3467d30e1797c0ff9840063fc1aa54fc63
daniel <gofmandon@gmail.com>
2022-12-31 21:28:10 +0300

    new screens

--- RaT2.0/client/lib/main.dart
+++ RaT2.0/client/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import './BottomNavBar/RentOffer.dart';
-import './BottomNavBar/RentRequest.dart';
+import 'Screens/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,47 +13,9 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       // Hide the debug banner
       debugShowCheckedModeBanner: false,
-      title: 'Kindacode.com',
+      title: 'RaT',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedScreenIndex = 0;
-  final List _screens = [
-    {"screen": const RentOffer(), "title": "Screen RentOffer"},
-    {"screen": const RentRequest(), "title": "Screen RentRequest"}
-  ];
-
-  void _selectScreen(int index) {
-    setState(() {
-      _selectedScreenIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_screens[_selectedScreenIndex]["title"]),
-      ),
-      body: _screens[_selectedScreenIndex]["screen"],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedScreenIndex,
-        onTap: _selectScreen,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'offer'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'request')
-        ],
-      ),
-    );
-  }
-}
--- RaT2.0/server/app.py
+++ RaT2.0/server/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from Auth.api import Auth
+from auth.api import auth
 
 app = Flask(__name__)
-app.register_blueprint(Auth)
+app.register_blueprint(auth)
 
 
 if __name__ == "__main__":
